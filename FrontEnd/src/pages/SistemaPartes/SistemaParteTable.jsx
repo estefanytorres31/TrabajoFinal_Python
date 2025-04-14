49abe77ac7a27b8137f6a24476d91e9164eb3f9e
davidllanos31 <davidllanos.compina@gmail.com>
2025-04-14 15:49:20 -0500

    🚀 RELEASE:

--- FrontEnd/src/pages/SistemaPartes/SistemaParteTable.jsx
+++ FrontEnd/src/pages/SistemaPartes/SistemaParteTable.jsx
@@ -7,9 +7,9 @@ function SistemaParteTable({ sistemaPartes, onEdit, onDelete }) {
       <table className="min-w-full bg-white  rounded-md shadow">
         <thead className="bg-gray-800 text-white">
           <tr>
-            <th className="px-4 py-3 ">ID</th>
-            <th className="px-4 py-3 ">ID Sistema</th>
-            <th className="px-4 py-3 ">ID Parte</th>
+            <th className="px-4 py-3 ">ID Relación</th>
+            <th className="px-4 py-3 ">Sistema</th>
+            <th className="px-4 py-3 ">Parte</th>
             <th className="px-4 py-3  text-center">Acciones</th>
           </tr>
         </thead>
@@ -17,8 +17,8 @@ function SistemaParteTable({ sistemaPartes, onEdit, onDelete }) {
           {sistemaPartes.map((item) => (
             <tr key={item.id_sistema_parte} className=" hover:bg-gray-100">
               <td className="px-4 py-3 ">{item.id_sistema_parte}</td>
-              <td className="px-4 py-3 ">{item.id_sistema}</td>
-              <td className="px-4 py-3 ">{item.id_parte}</td>
+              <td className="px-4 py-3 ">{item.nombre_sistema}</td>
+              <td className="px-4 py-3 ">{item.nombre_parte}</td>
               <td className="px-4 py-3  flex justify-center gap-3">
                 <button
                   onClick={() => onEdit(item)}
