044e928630d64188a5d453b842cfe432d481ac2e
davidllanos31 <davidllanos.compina@gmail.com>
2025-04-14 13:15:09 -0500

    🐛 FIX:

--- FrontEnd/src/pages/SistemaPartes/SistemaParteTable.jsx
+++ FrontEnd/src/pages/SistemaPartes/SistemaParteTable.jsx
@@ -4,31 +4,31 @@ import { FiEdit2, FiTrash2 } from 'react-icons/fi';
 function SistemaParteTable({ sistemaPartes, onEdit, onDelete }) {
   return (
     <div className="overflow-x-auto">
-      <table className="min-w-full bg-white border rounded-md shadow">
+      <table className="min-w-full bg-white  rounded-md shadow">
         <thead className="bg-gray-800 text-white">
           <tr>
-            <th className="px-4 py-3 border">ID</th>
-            <th className="px-4 py-3 border">ID Sistema</th>
-            <th className="px-4 py-3 border">ID Parte</th>
-            <th className="px-4 py-3 border text-center">Acciones</th>
+            <th className="px-4 py-3 ">ID</th>
+            <th className="px-4 py-3 ">ID Sistema</th>
+            <th className="px-4 py-3 ">ID Parte</th>
+            <th className="px-4 py-3  text-center">Acciones</th>
           </tr>
         </thead>
         <tbody>
           {sistemaPartes.map((item) => (
-            <tr key={item.id_sistema_parte} className="border-b hover:bg-gray-100">
-              <td className="px-4 py-3 border">{item.id_sistema_parte}</td>
-              <td className="px-4 py-3 border">{item.id_sistema}</td>
-              <td className="px-4 py-3 border">{item.id_parte}</td>
-              <td className="px-4 py-3 border flex justify-center gap-3">
+            <tr key={item.id_sistema_parte} className=" hover:bg-gray-100">
+              <td className="px-4 py-3 ">{item.id_sistema_parte}</td>
+              <td className="px-4 py-3 ">{item.id_sistema}</td>
+              <td className="px-4 py-3 ">{item.id_parte}</td>
+              <td className="px-4 py-3  flex justify-center gap-3">
                 <button
                   onClick={() => onEdit(item)}
-                  className="p-2 rounded border hover:bg-blue-100 text-blue-900"
+                  className="p-2 rounded  hover:bg-blue-100 text-blue-900"
                 >
                   <FiEdit2 />
                 </button>
                 <button
                   onClick={() => onDelete(item.id_sistema_parte)}
-                  className="p-2 rounded border hover:bg-red-100 text-red-600"
+                  className="p-2 rounded  hover:bg-red-100 text-red-600"
                 >
                   <FiTrash2 />
                 </button>
